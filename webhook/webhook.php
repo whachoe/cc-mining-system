beb03f9a691ed98339c6428f25961da0fa6eccf9
Jo Giraerts <whachoe@gmail.com>
2026-08-25 19:12:00 +0200

    webhook fix

--- webhook/webhook.php
+++ webhook/webhook.php
@@ -1,9 +1,8 @@
 <?php
 
-var_dump($_REQUEST);
+//var_dump($_REQUEST);
 
 
 // Get the body of the request and print it on the screen
-echo "Body:";
-echo file_get_contents('php://input');
+file_put_contents('mc.log', file_get_contents('php://input'), FILE_APPEND);
 
